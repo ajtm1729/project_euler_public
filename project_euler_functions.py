ff25e71048193d47b34123e936337215cb68a465
ajtm1729 <a.j.macarthur@sms.ed.ac.uk>
2024-12-23 21:53:02 +0000

    solved problems 19 and 20

---
+++ project_euler_functions.py
@@ -0,0 +1,6 @@
+'''pe.py module contains functions for solving Project Euler problems
+
+This module contains functions I have written for solving some of the first 100
+Project Euler problems.
+'''
+
